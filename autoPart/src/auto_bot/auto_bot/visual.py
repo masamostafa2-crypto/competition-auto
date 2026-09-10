df387466ab20f75a19a085ac89f130934c0ae5a3
MASA <masamostafa2017@gmail.com>
2026-09-10 21:55:40 +0300

    added cv2 import

--- autoPart/src/auto_bot/auto_bot/visual.py
+++ autoPart/src/auto_bot/auto_bot/visual.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from ultralytics import YOLO
-
+import cv2
 
 class YoloVisual(Node):
     def __init__(self):
